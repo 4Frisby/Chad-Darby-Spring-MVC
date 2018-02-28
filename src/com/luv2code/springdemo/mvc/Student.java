1ec9c5512c52243b96caec3454cd759fdac0b5bb
Tarık Nural <tariknural00@gmail.com>
2018-03-01 02:15:36 +0300

    solution-code-spring-mvc-form-tags-drop-down-lists

--- src/com/luv2code/springdemo/mvc/Student.java
+++ src/com/luv2code/springdemo/mvc/Student.java
@@ -11,10 +11,6 @@ public class Student {
 	
 	private LinkedHashMap<String, String> countryOptions;
 	
-	private String favoriteLanguage;
-	
-	private String[] operatingSystems;
-	
 	public Student() {
 		
 		// populate country options: used ISO country code
@@ -55,21 +51,4 @@ public class Student {
 	public LinkedHashMap<String, String> getCountryOptions() {
 		return countryOptions;
 	}
-
-	public String getFavoriteLanguage() {
-		return favoriteLanguage;
-	}
-
-	public void setFavoriteLanguage(String favoriteLanguage) {
-		this.favoriteLanguage = favoriteLanguage;
-	}
-
-	public String[] getOperatingSystems() {
-		return operatingSystems;
-	}
-
-	public void setOperatingSystems(String[] operatingSystems) {
-		this.operatingSystems = operatingSystems;
-	}
-			
 }
